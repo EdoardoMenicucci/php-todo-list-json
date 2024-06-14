a8208ce69b92ba83222c50af0348293d1186efea
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-06-14 15:24:37 +0200

    esercizio completo

--- js/main.js
+++ js/main.js
@@ -4,13 +4,32 @@ createApp({
   data() {
     return {
       todolist: "",
+      newTask: "",
+      config: {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      },
     };
   },
-  methods: {},
+  methods: {
+    addTask() {
+      const task = {
+        cosa: this.newTask,
+        stato: "notDone",
+      };
+      axios.post("server.php", task, this.config).then((result) => {
+        console.log(result, task);
+        //aggiorno la lista con i dati aggiornati con la nuova task
+        this.todolist = result.data;
+      });
+    },
+  },
   mounted() {
     // Chiamata axios verso il mio server (l'indirizzo del server si basa dalla cartella dell'index)
     axios.get("server.php").then((result) => {
       this.todolist = result.data;
+      console.log(result.data);
     });
   },
 }).mount("#app");
--- server.php
+++ server.php
@@ -1,11 +1,38 @@
 <?php
+//RECUPERO I DATI IN FORMATO JSON
+$dati = file_get_contents("data.json");
+$newData = '';
 
-$todo = ['Fare la spesa', 'Lavare i piatti', 'Fare da mangiare per il cane', 'Portare a spasso il cane', 'Mangiare', 'Dormire', 'Studiare', 'Fare la doccia', 'Passeggiare'];
 
-$todoEncoded = json_encode($todo);
+if (isset($_POST['cosa'])) { // SE RICEVO DATI IN POST LI AGGIORNO AL DATA.JSON
 
-// Specifico il tipo di contenuto che verra ricevuto tramite API
-header('Content-Type: application/json');
+    //converto il json in un array associativo php
+    $allTask = json_decode($dati, true);
 
-//Specifico il contenuto da esportare
-echo $todoEncoded;
+    //Prendo la task aggiunta e creo un oggetto come gli altri
+    $newTask = [
+        "cosa" => $_POST['cosa'],
+        "stato" => 'notDone'
+    ];
+
+    //pusho la task nell array preso dal data.json
+    $allTask[] = $newTask;
+
+    //riconverto il file in JSON
+    $newData = json_encode($allTask);
+
+    // Pusho il file riconvertito nel data.json
+    file_put_contents("data.json", $newData);
+
+    //Specifico il tipo di dati inviati
+    header('Content-Type: application/json');
+
+    echo $newData;
+} else { //SE NON RICEVO DATI IN POST MOSTRO LA LISTA
+
+    // Specifico il tipo di contenuto che verra ricevuto tramite API
+    header('Content-Type: application/json');
+
+    //Specifico il contenuto da esportare
+    echo $dati;
+}
